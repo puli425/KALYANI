06556fec0109434c616585bf0f9ac03a5cb907b4
puli425 <41765791+puli425@users.noreply.github.com>
2018-08-20 16:24:09 +0530

    Update alphabet.py

--- beginer/alphabet.py
+++ beginer/alphabet.py
@@ -1,6 +1,6 @@
 ch=raw_input()
 str=('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
-if (ch in str):
+if(ch in str):
 	print("Alphabet")
 else:
 	print("No")
